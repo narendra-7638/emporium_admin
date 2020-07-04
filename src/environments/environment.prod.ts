e197d8d6cef22b15919f4c1cd4cc46795dba9d20
Narendra Vishwakarma <narendra@studiographene.com>
2020-07-04 20:21:44 +0530

    just commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true
 };
+
+
+export const baseUrl = "http://18.222.143.146:3000/api/";
+
+
+export const s3Creds = {
+  accessKeyId: 'AKIAWO4MZZPDBYRX5FGF',
+  secretAccessKey: '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',
+  region: 'ap-south-1',
+  bucket: 'mediafolderapp'
+}
